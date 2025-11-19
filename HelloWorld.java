28878e5551952c80a3b76f7d9c27cd3dfeb387f0
varnikanalla <nallavarnika@gmail.com>
2025-11-19 22:09:43 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// This is a simple Java program
+// It prints "Hello, World!" on the screen
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
